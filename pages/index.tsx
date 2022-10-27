4bcd8beb1aa01facc3501c4f14e8c5fa1ac0da5e
Jason <jason@Jasons-MBP.hsd1.or.comcast.net>
2022-10-26 23:01:44 -0700

    UI done

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,7 @@ const Home: NextPage = ({ exploreData, cardsData }: any) => {
           <h2 className='text-3xl font-semibold py-8'>Live Anywhere</h2>
 
           {/* pull some data from a server */}
+          {/* scrollbar-hide see tailwind.config.js plugin */}
           <div className="flex  space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardsData.map((item: any) => (
               <MediumCard
